a8c52ecb04917d151b8c1ce676707e0d4810220e
EasterTheBunny <athughlett@gmail.com>
2025-03-25 20:30:13 -0500

    signal lint corrections

--- internal/signal/signal.go
+++ internal/signal/signal.go
@@ -15,39 +15,46 @@ type config struct {
 	channel channels.Channel
 }
 
+const (
+	DefaultChannel = "operator://rtctunnel-operator.fly.dev"
+)
+
+//nolint:gochecknoglobals
 var defaultOptions = []Option{
-	WithChannel(channels.Must(channels.Get("operator://rtctunnel-operator.fly.dev"))),
+	WithChannel(channels.Must(channels.Get(DefaultChannel))),
 }
 
 func getConfig(options ...Option) (*config, error) {
 	cfg := new(config)
-	for _, o := range defaultOptions {
-		err := o(cfg)
-		if err != nil {
+
+	for _, opt := range defaultOptions {
+		if err := opt(cfg); err != nil {
 			return nil, err
 		}
 	}
-	for _, o := range options {
-		err := o(cfg)
-		if err != nil {
+
+	for _, opt := range options {
+		if err := opt(cfg); err != nil {
 			return nil, err
 		}
 	}
+
 	return cfg, nil
 }
 
-// An Option customizes the config.
+// Option customizes the config.
 type Option func(cfg *config) error
 
 // WithChannel sets the channel option.
 func WithChannel(ch channels.Channel) Option {
 	return func(cfg *config) error {
 		cfg.channel = ch
+
 		return nil
 	}
 }
 
-// SetDefaultOptions sets the default options
+// SetDefaultOptions sets the default options.
 func SetDefaultOptions(options ...Option) {
 	defaultOptions = options
 }
@@ -58,30 +65,37 @@ func Send(ctx context.Context, keypair crypt.KeyPair, peerPublicKey crypt.Key, d
 	if err != nil {
 		return err
 	}
+
 	encrypted := keypair.Encrypt(peerPublicKey, data)
 	address := peerPublicKey.String() + "/" + keypair.Public.String()
 	encoded := base58.Encode(encrypted)
+
 	return cfg.channel.Send(ctx, address, encoded)
 }
 
 // Recv receives a message from a peer. Messages are encrypted and authenticated.
-func Recv(ctx context.Context, keypair crypt.KeyPair, peerPublicKey crypt.Key, options ...Option) (data []byte, err error) {
+func Recv(ctx context.Context, keypair crypt.KeyPair, peerPublicKey crypt.Key, options ...Option) ([]byte, error) {
 	cfg, err := getConfig(options...)
 	if err != nil {
 		return nil, err
 	}
+
 	address := keypair.Public.String() + "/" + peerPublicKey.String()
+
 	encoded, err := cfg.channel.Recv(ctx, address)
 	if err != nil {
 		return nil, err
 	}
+
 	decoded, err := base58.Decode(encoded)
 	if err != nil {
 		return nil, err
 	}
+
 	decrypted, err := keypair.Decrypt(peerPublicKey, decoded)
 	if err != nil {
 		return nil, err
 	}
+
 	return decrypted, nil
 }
