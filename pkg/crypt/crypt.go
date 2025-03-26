887158b4e9cab9fd1cb4dd142bf11ece180600e7
EasterTheBunny <athughlett@gmail.com>
2025-03-25 20:43:55 -0500

    crypt lint corrections

--- pkg/crypt/crypt.go
+++ pkg/crypt/crypt.go
@@ -11,116 +11,137 @@ import (
 )
 
 const (
-	// KeySize is the size of an encryption key in bytes
+	// KeySize is the size of an encryption key in bytes.
 	KeySize = 32
-	// NonceSize is the size of a nonce in bytes
+	// NonceSize is the size of a nonce in bytes.
 	NonceSize = 24
 )
 
 type (
-	// Key is a public or private encryption key
+	// Key is a public or private encryption key.
 	Key [KeySize]byte
-	// A KeyPair is a public, private key pair
+	// A KeyPair is a public, private key pair.
 	KeyPair struct {
 		Public, Private Key
 	}
-	// Nonce is a number used once
+	// Nonce is a number used once.
 	Nonce = [NonceSize]byte
 )
 
-// NewKey creates a new key from a base58 string
-func NewKey(str string) (key Key, err error) {
-	bs, err := base58.Decode(str)
+// NewKey creates a new key from a base58 string.
+func NewKey(str string) (Key, error) {
+	var key Key
+
+	bts, err := base58.Decode(str)
 	if err != nil {
 		return key, err
 	}
-	if len(bs) != KeySize {
+
+	if len(bts) != KeySize {
 		return key, errors.New("invalid key")
 	}
-	copy(key[:], bs)
+
+	copy(key[:], bts)
+
 	return key, nil
 }
 
-func (key Key) Valid() bool {
-	return [KeySize]byte(key) != [KeySize]byte{}
+func (k Key) Valid() bool {
+	return [KeySize]byte(k) != [KeySize]byte{}
 }
 
-func (key Key) String() string {
-	return base58.Encode(key[:])
+func (k Key) String() string {
+	return base58.Encode(k[:])
 }
 
-func (key Key) MarshalJSON() ([]byte, error) {
-	bs, err := json.Marshal(key.String())
+func (k Key) MarshalJSON() ([]byte, error) {
+	bts, err := json.Marshal(k.String())
 	if err != nil {
 		return nil, err
 	}
-	return bs, nil
+
+	return bts, nil
 }
 
-func (key *Key) UnmarshalJSON(data []byte) error {
-	var raw string
-	err := json.Unmarshal(data, &raw)
-	if err != nil {
+func (k *Key) UnmarshalJSON(data []byte) error {
+	var (
+		raw string
+		err error
+	)
+
+	if err = json.Unmarshal(data, &raw); err != nil {
 		return err
 	}
-	*key, err = NewKey(raw)
-	if err != nil {
+
+	if *k, err = NewKey(raw); err != nil {
 		return err
 	}
+
 	return nil
 }
 
-func (key Key) MarshalYAML() (interface{}, error) {
-	return key.String(), nil
+func (k Key) MarshalYAML() (any, error) {
+	return k.String(), nil
 }
 
-func (key *Key) UnmarshalYAML(unmarshal func(interface{}) error) error {
-	var str string
-	err := unmarshal(&str)
-	if err != nil {
+func (k *Key) UnmarshalYAML(unmarshal func(any) error) error {
+	var (
+		str string
+		err error
+	)
+
+	if err = unmarshal(&str); err != nil {
 		return err
 	}
-	*key, err = NewKey(str)
-	if err != nil {
+
+	if *k, err = NewKey(str); err != nil {
 		return err
 	}
+
 	return nil
 }
 
-// GenerateKeyPair generates a (public, private) encryption key
+// GenerateKeyPair generates a (public, private) encryption key.
 func GenerateKeyPair() KeyPair {
 	pub, priv, err := box.GenerateKey(rand.Reader)
 	if err != nil {
 		panic(err)
 	}
+
 	return KeyPair{Public: *pub, Private: *priv}
 }
 
-// Encrypt encrypts a message using a peer's public key and the local private key
-func (pair *KeyPair) Encrypt(peerPublicKey Key, data []byte) []byte {
+// Encrypt encrypts a message using a peer's public key and the local private key.
+func (p *KeyPair) Encrypt(peerPublicKey Key, data []byte) []byte {
 	nonce := generateNonce()
 	k1 := [KeySize]byte(peerPublicKey)
-	k2 := [KeySize]byte(pair.Private)
+	k2 := [KeySize]byte(p.Private)
 	sealed := box.Seal(nil, data, &nonce, &k1, &k2)
 
 	var result []byte
+
 	result = append(result, nonce[:]...)
 	result = append(result, sealed...)
+
 	return result
 }
 
-// Decrypt decrypts a message using a peer's public key and the local private key
-func (pair *KeyPair) Decrypt(peerPublicKey Key, data []byte) ([]byte, error) {
+// Decrypt decrypts a message using a peer's public key and the local private key.
+func (p *KeyPair) Decrypt(peerPublicKey Key, data []byte) ([]byte, error) {
 	if len(data) < NonceSize {
 		return nil, errors.New("invalid message")
 	}
+
 	var nonce Nonce
+
 	copy(nonce[:], data)
+
 	sealed := data[NonceSize:]
 	k1 := [KeySize]byte(peerPublicKey)
-	k2 := [KeySize]byte(pair.Private)
-	opened, ok := box.Open(nil, sealed, &nonce, &k1, &k2)
-	if !ok {
+	k2 := [KeySize]byte(p.Private)
+
+	opened, succeeded := box.Open(nil, sealed, &nonce, &k1, &k2)
+	if !succeeded {
 		return nil, errors.New("invalid message")
 	}
 
@@ -129,8 +150,10 @@ func (pair *KeyPair) Decrypt(peerPublicKey Key, data []byte) ([]byte, error) {
 
 func generateNonce() Nonce {
 	var nonce Nonce
+
 	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
 		panic(err)
 	}
+
 	return nonce
 }
--- pkg/crypt/crypt_test.go
+++ pkg/crypt/crypt_test.go
@@ -1,19 +1,24 @@
-package crypt
+package crypt_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/rtctunnel/rtctunnel/pkg/crypt"
 )
 
-func Test(t *testing.T) {
-	k1 := GenerateKeyPair()
-	k2 := GenerateKeyPair()
+func TestCrypt(t *testing.T) {
+	t.Parallel()
+
+	k1 := crypt.GenerateKeyPair()
+	k2 := crypt.GenerateKeyPair()
 
 	msg := []byte("Hello World")
-
 	encrypted := k1.Encrypt(k2.Public, msg)
 	decrypted, err := k2.Decrypt(k1.Public, encrypted)
-	assert.NoError(t, err)
+
+	require.NoError(t, err)
 	assert.Equal(t, msg, decrypted)
 }
